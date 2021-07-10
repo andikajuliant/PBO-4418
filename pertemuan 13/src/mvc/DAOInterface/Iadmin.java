32150a65014fd02690ef3dd621dc463fc2ea9328
andikajuliant <79988531+andikajuliant@users.noreply.github.com>
2021-07-10 23:52:39 +0700

    Update Iadmin.java

--- pertemuan 13/src/mvc/DAOInterface/Iadmin.java
+++ pertemuan 13/src/mvc/DAOInterface/Iadmin.java
@@ -1,9 +1,12 @@
-public class ladmin {
+package mvc.DAOInterface;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
-    }
+import java.util.List;
+import mvc.model.admin;
+
+public interface Iadmin {
+    public void insert(Admin u);
+    public void update(Admin u);
+    public void delete(String identitas);
+    public List<Admin> getAll();
+    public List<Admin> getCariNama(String nama);
 }
