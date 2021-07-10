d5889512f5aae0a7df1912914d1ea72d1026a6d9
andikajuliant <79988531+andikajuliant@users.noreply.github.com>
2021-07-10 23:56:07 +0700

    Update tabelmodeladmin.java

--- pertemuan 13/src/mvc/model/tabelmodeladmin.java
+++ pertemuan 13/src/mvc/model/tabelmodeladmin.java
@@ -1,9 +1,66 @@
-public class tabelmodelzdmin {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+package mvc.model;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+/**
+ *
+ * @author Raka Apritama
+ */
+public class tabelmodeladmin extends AbstractTableModel{
+    List<Admin> lb;
+    
+    public tabelmodeladmin(List<Admin> lb){
+        this.lb = lb;
+    }
+    
+    @Override
+    public int getColumnCount(){
+        return 7;
+    }
+    
+    public int getRowCount(){
+        return lb.size();
+    }
+    
+    public String getColumnName(int column){
+        switch (column){
+            case 0:
+                return "ID";
+            case 1:
+                return "NIK";
+            case 2:
+                return "Nama";
+            case 3:
+                return "Jenis Kelamin";
+            case 4:
+                return "Alamat";
+            case 5:
+                return "Usia";
+            case 6:
+                return "Alasan";
+            default:
+                return null;
+        }
+    }
+    
+    @Override
+    public Object getValueAt(int row, int column){
+        switch (column){
+            case 0:
+                return lb.get(row).getId();
+            case 1:
+                return lb.get(row).getNIK();
+            case 2:
+                return lb.get(row).getNama();
+            case 3:
+                return lb.get(row).getJk();
+            case 4:
+                return lb.get(row).getAlamat();
+            case 5:
+                return lb.get(row).getUsia();
+            case 6:
+                return lb.get(row).getAlasan();
+            default:
+                return null;
+        }
     }
 }
