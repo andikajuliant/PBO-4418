976e30abf33de767918f9b47c2d1dafa458f0348
andikajuliant <79988531+andikajuliant@users.noreply.github.com>
2021-07-10 23:55:29 +0700

    Update admin.java

--- pertemuan 13/src/mvc/model/admin.java
+++ pertemuan 13/src/mvc/model/admin.java
@@ -1,9 +1,114 @@
+package mvc.model;
 public class admin {
 
     /**
-     * @param args the command line arguments
+     * @return the id
      */
-    public static void main(String args[]) {
-        // TODO code application logic here
+    public Integer getId() {
+        return id;
     }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nik
+     */
+    public String getNIK() {
+        return nik;
+    }
+
+    /**
+     * @param nik the nik to set
+     */
+    public void setNIK(String nik) {
+        this.nik = nik;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the jk
+     */
+    public String getJk() {
+        return jk;
+    }
+
+    /**
+     * @param jk the jk to set
+     */
+    public void setJk(String jk) {
+        this.jk = jk;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the usia
+     */
+    public Integer getUsia() {
+        return usia;
+    }
+
+    /**
+     * @param usia the usia to set
+     */
+    public void setUsia(Integer usia) {
+        this.usia = usia;
+    }
+
+    /**
+     * @return the alasan
+     */
+    public String getAlasan() {
+        return alasan;
+    }
+
+    /**
+     * @param alasan the alasan to set
+     */
+    public void setAlasan(String alasan) {
+        this.alasan = alasan;
+    }
+
+    /**
+     * @return the id
+     */
+    
+    private Integer id;
+    private String nik;
+    private String nama;
+    private String jk;
+    private String alamat;
+    private Integer usia;
+    private String alasan;
+    
 }
